f26bff2aa769a3d44508c01c1479fb2265492317
Martin <martin@cardiologs.com>
2021-12-18 16:52:00 +0100

    🎉 init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Alerts } from "./models";
+
+const alert: Alerts = {
+  id: "id",
+  name: "alert",
+  created_at: 1,
+};
+
+console.log("hello world", alert);
---
+++ src/models.ts
@@ -0,0 +1,5 @@
+export interface Alerts {
+  id: string;
+  name: string;
+  created_at: number;
+}
